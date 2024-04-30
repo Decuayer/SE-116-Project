3e11d6c1637786bab1d49822db2840c1f85c29e7
Demir Cücü <demircucu35@gmail.com>
2024-04-30 20:47:35 +0300

    Changes by demir

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test 2");
+        System.out.println("Test 2"); 
+        System.out.println("Alp Salın Kalın Bir Adam");
     }
 }
