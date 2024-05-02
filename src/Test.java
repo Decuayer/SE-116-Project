4e798dad23e0223dd288c1dfb3e141e5e104433b
Demir Cücü <demircucu35@gmail.com>
2024-05-02 20:07:05 +0300

    Changes by alpsa

--- src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Test 2"); 
-        System.out.println("Alp Salın Kalın Bir Adam");
-        System.out.println("Baba bunun kafası bir başka");
-    }
-}
